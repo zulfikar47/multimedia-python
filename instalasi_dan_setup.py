b923aead66d7e070fd276a2b2611082e226183d2
zulfikar47 <138555820+zulfikar47@users.noreply.github.com>
2024-08-01 03:22:24 +0000

    chapter 1 selesai

---
+++ instalasi_dan_setup.py
@@ -0,0 +1,10 @@
+import os
+os.environ['SDL_AUDIODRIVER'] = 'dsp'
+import pygame
+import PIL
+import cv2
+import moviepy.editor as mp
+import pydub
+import tkinter as tk
+
+print("✅ Semua pustaka telah terinstal dengan benar!")
